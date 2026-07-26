32fccef07c358ca6a3e2a33ecaddd52614a14304
houssam20-M <houssamoubaha17@gmail.com>
2026-07-26 20:05:57 +0100

    Initial commit

---
+++ C02/ex09/ft_strcapitalize.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+char *ft_strlowcase(char *str)
+{
+    int i = 0;
+
+    while(str[i])
+    {
+        if(str[i] >= 'A' && str[i] <= 'Z')
+        {
+            str[i] = str[i] + 32;
+        }
+        i++;
+    }
+    return (str);
+}
+
+char *ft_strcapitalize(char *str)
+{
+    int i = 0;
+    int j = 1;
+
+    ft_strlowcase(str);
+    while(str[i])
+    {
+        if(str[i] >= 'a' && str[i] <= 'z')
+        {
+            if(j == 1)
+            {
+            str[i] = str[i] - 32;
+            j = 0;
+            }
+        }
+        else if(str[i] >= '0' && str[i] <= '9')
+        {
+            j = 0;
+        }
+        else
+        {
+            j = 1;
+        }
+        i++;
+    }
+    return (str);
+}
